ec97918a16116a51668b5c399fc89dca7b73652c
poorvaditya behre <aditya.behre@gmail.com>
2023-01-26 11:23:27 +0530

    added digitSummation

--- Cpp-Basics-1/problem-DigitSummation.cpp
+++ Cpp-Basics-1/problem-DigitSummation.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
+#define ll long long int 
 using namespace std;
+
+ll lastDigit(ll num)
+{
+  return num%10;
+}
 
 int main()
 {
+    ll a,b;
+    ll sum=0;
+    cin>>a>>b;
+    sum = lastDigit(a) +lastDigit(b);
+    cout<<sum<<endl;
+    
     return 0;
 }
