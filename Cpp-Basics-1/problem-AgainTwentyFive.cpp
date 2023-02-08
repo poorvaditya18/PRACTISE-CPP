5fba0df730298dd2c617eaacf26c589bda5d1999
poorvaditya behre <aditya.behre@gmail.com>
2023-02-08 09:09:11 +0530

    solved again25

---
+++ Cpp-Basics-1/problem-AgainTwentyFive.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif	
+	
+
+	ll n;
+	cin>>n;
+	cout<<25<<endl;
+ 
+	return 0;
+
+}
