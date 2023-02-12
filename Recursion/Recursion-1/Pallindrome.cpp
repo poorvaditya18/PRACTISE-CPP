8a3f811f5c6ec4e6563519f9d5359edf3f1b3390
poorvaditya behre <aditya.behre@gmail.com>
2023-02-12 12:01:47 +0530

    solved Pallindrome

---
+++ Recursion/Recursion-1/Pallindrome.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool Func(int num , int *temp)
+{
+   
+    //base case 
+    if(num>=0 and num<=9)
+    {
+    	int last_digit = (*temp)%10;
+    	(*temp)/=10;
+    	return (num==last_digit);
+    }
+
+   bool res = (Func(num/10,temp) and (num%10==(*temp)%10));
+   (*temp) /= 10;
+    return res;
+}
+
+
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif	
+	
+    // To check whether a given number is pallindrome or not 
+    
+	int t;
+	cin>>t;
+	while(t>0)
+	{
+		int n;
+		cin>>n;
+		int anotherTemp= n;
+		int *temp = &anotherTemp;
+		if(Func(n,temp))
+		{
+			cout<<"Yes"<<endl;
+		}
+		else
+		{
+			cout<<"No"<<endl;
+		}
+		t--;
+	}
+ 
+	return 0;
+
+}
