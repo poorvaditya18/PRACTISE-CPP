3fb0a3f49c87052743a2011343f3045f71ae4fc9
poorvaditya behre <aditya.behre@gmail.com>
2023-02-17 20:56:46 +0530

    solved Hexadecimal Theorem

---
+++ Problem-Solving/part-0/problem-HexadecimalTheorem.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int 
+
+
+
+
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif	
+	
+  ll n;
+  cin>>n;
+
+  cout<<0<<" "<<0<<" "<<n<<endl;
+
+
+	return 0;
+
+}
