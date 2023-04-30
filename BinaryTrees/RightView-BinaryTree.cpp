63199be8b79f379f69aff5c93e074f18b19df347
poorvaditya behre <aditya.behre@gmail.com>
2023-04-30 18:31:02 +0530

    solved right view Binary Tree

---
+++ BinaryTrees/RightView-BinaryTree.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+ 
+class Solution {
+public:
+
+	// we have used Recursive Reverse Preorder ->  ROOT , RIGHT , LEFT 
+    void func(TreeNode* root , int level, vector<int>& ans)
+    {
+        if(root==NULL) return ;
+
+        if(level==ans.size())
+        {
+            ans.push_back(root->val);
+        }
+        func(root->right,level+1,ans);
+        func(root->left,level+1,ans);
+    }
+    vector<int> rightSideView(TreeNode* root) 
+    {
+        vector<int> ans;
+        func(root,0,ans);
+        return ans;
+    }
+};
+int main()
+{
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif	
+	
+ 
+	return 0;
+
+}
